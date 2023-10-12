42c653c376232b855fddcf0c972d0df85aece2a1
Xavier Santos <up202108894@up.pt>
2023-10-12 10:00:25 +0100

    added general application layer execution outline

--- src/application_layer.c
+++ src/application_layer.c
@@ -2,8 +2,27 @@
 
 #include "application_layer.h"
 
+#include "link_layer.h" // in order to call transmission level functions
+
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename)
 {
     // TODO
+    //calls for opening of port
+
+    //IF TRANSMITTER
+    //loads 1024 bytes of that into array
+    //calls llwrite() for those bytes
+    //when llwrite() returns 1, loads another 1024 bytes and repeats
+    //if llwrite() returns -1, tries again with the same info
+    //repeat this process until it finishes
+
+    //when llwrite() returns succesfully for the last time, call llclose() and finish execution
+
+    //IF RECEIVER
+    //reads from array
+    //when array ends, calls for llread()
+    //if llread() returns 1, read from array into file, using state machine to determine type of data
+    //if llread() returns -1, try again, ignoring what already exists in the array
+    //when it reads the END control packet, calls for llclose() and finishes execution
 }
