38055296d48c63fcbbdacf217c68ec88cbbd6b98
Xavier Santos <up202108894@up.pt>
2023-10-12 09:36:01 +0100

    llopen and llclose

--- src/link_layer.c
+++ src/link_layer.c
@@ -2,8 +2,23 @@
 
 #include "link_layer.h"
 
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <termios.h>
+#include <unistd.h>
+#include <signal.h>
+
 // MISC
 #define _POSIX_SOURCE 1 // POSIX compliant source
+
+//isto fica de fora para ser acedido por todas as funções
+int fd;
+struct termios oldtio; // old port parameters
+struct termios newtio; // new port parameters
 
 ////////////////////////////////////////////////
 // LLOPEN
@@ -11,6 +26,44 @@
 int llopen(LinkLayer connectionParameters)
 {
     // TODO
+    fd = open(connectionParameters.serialPort, O_RDWR | O_NOCTTY);
+    if(fd < 0) {
+        perror(connectionParameters.serialPort);
+        exit(-1);
+    }
+
+    if(tcgetattr(fd, &oldtio) == -1) {
+        perror("tcgetattr");
+        exit(-1);
+    }
+
+    // Clear struct for new port settings
+    memset(&newtio, 0, sizeof(newtio));
+
+    newtio.c_cflag = connectionParameters.baudRate | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+
+    // Set input mode (non-canonical, no echo,...)
+    newtio.c_lflag = 0;
+    newtio.c_cc[VTIME] = 0; // value subject to change
+    newtio.c_cc[VMIN] = 5;  // value subject to change
+
+    // Now clean the line and activate the settings for the port
+    // tcflush() discards data written to the object referred to
+    // by fd but not transmitted, or data received but not read,
+    // depending on the value of queue_selector:
+    //   TCIFLUSH - flushes data received but not read.
+    tcflush(fd, TCIOFLUSH);
+
+    // Set new port settings
+    if (tcsetattr(fd, TCSANOW, &newtio) == -1)
+    {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+    printf("New termios structure set\n");
 
     return 1;
 }
@@ -41,6 +94,14 @@ int llread(unsigned char *packet)
 int llclose(int showStatistics)
 {
     // TODO
+    // Restore the old port settings
+    if (tcsetattr(fd, TCSANOW, &oldtio) == -1)
+    {
+        perror("tcsetattr");
+        exit(-1);
+    }
 
+    close(fd);
+    
     return 1;
 }
