ab8ad21d1afc2bf7e5a2f19784376dba4b9b09e7
Xavier Santos <up202108894@up.pt>
2023-10-14 14:24:57 +0100

    added more macros and built the state machine for llread() (incomplete)

--- src/link_layer.c
+++ src/link_layer.c
@@ -16,6 +16,9 @@
 #define _POSIX_SOURCE 1 // POSIX compliant source
 
 // macros
+#define TRUE 1
+#define FALSE 0
+
 #define FLAG 0x7E
 
 #define A_TC_RR 0x03 // A for Transmitter commands or Receiver replies
@@ -29,9 +32,28 @@
 #define C_REJ1 0x81 // C for Receiver to signal it rejects the last 1-type data message
 #define C_DISC 0x0B // C for DISC messages
 
+#define C_INFO_0 0x00 // C for Information messages with byte 0
+#define C_INFO_1 0x40 // C for Information messages with byte 1
+
 #define ESC_SEQUENCE 0x7D // escape sequence for byte stuffing
 #define ESC_5d 0x5D // if 0x7D in Data
 #define ESC_5e 0x5E // if 0x7E in Data
+
+#define START 0
+#define FLAG_1_OK 1
+#define A_OK 2
+#define A_UA_OK 3
+#define INFO_0 4
+#define INFO_1 5
+#define is_SET 6
+#define is_DISC 7
+#define C_UA_OK 8
+#define READ_INFO 9
+#define awaiting_FLAG 10
+#define awaiting_FLAG_DISC 11
+#define BCC_UA_OK 12
+#define got_7d 13
+#define STANDBY 14
 
 // isto fica de fora para ser acedido por todas as funções
 int fd;
@@ -102,6 +124,258 @@ int llwrite(const unsigned char *buf, int bufSize)
 int llread(unsigned char *packet)
 {
     // TODO
+    unsigned char STOP = FALSE;
+    unsigned char BCC2_calc = 0x00;
+    unsigned char state = START;
+    unsigned char buf[1] = {0};
+    unsigned char received_size = 0;
+    unsigned char index = 0;
+    unsigned char DISC = FALSE;
+    unsigned char SET = FALSE;
+    unsigned char can_receive_bit = 0x00;
+    unsigned char standby_byte;
+
+    while(STOP == FALSE) {
+        int byte = read(fd, buf, 1);
+        if(byte <= 0) continue;
+
+        switch(state) {
+            case START:
+                if(buf[0]==FLAG) {
+                    state = FLAG_1_OK;
+                }
+                break;
+            
+            case FLAG_1_OK:
+                if(buf[0]==FLAG) {
+                    state = FLAG_1_OK;
+                }
+                else if(buf[0]==A_TC_RR) {
+                    state = A_OK;
+                }
+                else if(DISC==TRUE && buf[0]==A_RC_TR) {
+                    state = A_UA_OK;
+                }
+                else {
+                    state = START;
+                }
+                break;
+            case A_OK:
+                if(buf[0]==FLAG) {
+                    state = FLAG_1_OK;
+                }
+                else if(SET==TRUE && (buf[0]==C_INFO_0 && can_receive_bit==C_INFO_0)) {
+                    state = INFO_0;
+                }
+                else if(SET==TRUE && (buf[0]==C_INFO_1 && can_receive_bit==C_INFO_1)) {
+                    state = INFO_1;
+                }
+                else if(buf[0]==C_SET) {
+                    state = is_SET;
+                }
+                else if(buf[0]==C_DISC) {
+                    state = is_DISC;
+                }
+                else {
+                    state = START;
+                }
+                break;
+            case A_UA_OK:
+                if(buf[0]==FLAG) {
+                    state = FLAG_1_OK;
+                }
+                else if(buf[0]==C_UA) {
+                    state = C_UA_OK;
+                }
+                else {
+                    state = START;
+                }
+                break;
+            case INFO_0:
+                if(buf[0]==FLAG) {
+                    state = FLAG_1_OK;
+                }
+                else if(buf[0]==0x03) {
+                    state = READ_INFO;
+                }
+                else {
+                    state = START;
+                }
+                break;
+            case INFO_1:
+                if(buf[0]==FLAG) {
+                    state = FLAG_1_OK;
+                }
+                else if(buf[0]==0x43) {
+                    state = READ_INFO;
+                }
+                else {
+                    state = START;
+                }
+                break;
+            case is_SET:
+                if(buf[0]==FLAG) {
+                    state = FLAG_1_OK;
+                }
+                else if(buf[0]=0x00) {
+                    state = awaiting_FLAG;
+                }
+                else {
+                    state = START;
+                }
+                break;
+            case is_DISC:
+                if(buf[0]==FLAG) {
+                    state = FLAG_1_OK;
+                }
+                else if(buf[0]==0x08) {
+                    state = awaiting_FLAG_DISC;
+                }
+                else {
+                    state = START;
+                }
+                break;
+            case C_UA_OK:
+                if(buf[0]==FLAG) {
+                    state = FLAG_1_OK;
+                }
+                else if(buf[0]==0x06) {
+                    state = BCC_UA_OK;
+                }
+                else {
+                    state = START;
+                }
+                break;
+            case READ_INFO:
+                if(buf[0]==FLAG) {
+                    state = START;
+                    STOP = TRUE;
+                    index = 0;
+                    //send REJECT message
+                }
+                else if(buf[0]==BCC2_calc) {
+                    state = STANDBY;
+                    standby_byte = buf[0];
+                }
+                else if(buf[0]==ESC_SEQUENCE) {
+                    state = got_7d;
+                }
+                else {
+                    state = READ_INFO;
+                    packet[index] = buf[0];
+                    index++;
+                    BCC2_calc = BCC2_calc ^ buf[0];
+                }
+                break;
+            case got_7d:
+                if(buf[0]==ESC_5d) {
+                    if(BCC2_calc == 0x7D) {
+                        state = STANDBY; // BCC2 could be 0x7D, in which case we need to check for  0x7d 0x5d 0x7e
+                        standby_byte = ESC_5d;
+                    }
+                    else {
+                       state = READ_INFO;
+                        packet[index] = 0x7D;
+                        index++;
+                        BCC2_calc = BCC2_calc ^ 0x7D; 
+                    }   
+                }
+                else if(buf[0]==ESC_5e) {
+                    if(BCC2_calc == 0x7E) {
+                        state = STANDBY; // BCC2 could be 0x7E, in which case we need to check for 0x7d 0x5e 0x7e
+                        standby_byte = ESC_5e;
+                    }
+                    else {
+                        state = READ_INFO;
+                        packet[index] = 0x7E;
+                        index++;
+                        BCC2_calc = BCC2_calc ^ 0x7E;
+                    }                    
+                }
+                else {
+                    STOP = TRUE;
+                    state = START;
+                    index = 0;
+                    // need to reject message and wait for new one
+                }
+            case STANDBY:
+                if(standby_byte==ESC_5d) {
+                    if(buf[0]==FLAG) {
+                        STOP = TRUE;
+                        can_receive_bit = can_receive_bit ^ 0x40;
+                        // BCC2 was 0x7D, and there was nothing wrong with the message; we can send RR and return
+                    }
+                    else { // didn't receive F, which means 0x7D was NOT the BCC2 byte, and therefore we have to send 0x7D to the packet
+                        packet[index] = 0x7D;
+                        index++;
+                        if(buf[0]==ESC_SEQUENCE) {
+                            state = got_7d;
+                            BCC2_calc = 0x00;
+                        }
+                        else {
+                            packet[index] = buf[0];
+                            BCC2_calc = buf[0];
+                            index++;
+                            state = READ_INFO;
+                        }
+                    }
+                }
+                else if(standby_byte==ESC_5e) {
+                    if(buf[0]==FLAG) {
+                        STOP = TRUE;
+                        // BCC2 was 0x7E, and there was nothing wrong with the message; we can send RR and return
+                    }
+                    else { // didn't receive F, which means 0x7E was NOT the BCC2 byte, and therefore we have to send 0x7E to the packet
+                        packet[index] = 0x7E;
+                        index++;
+                        if(buf[0]==ESC_SEQUENCE) {
+                            state = got_7d;
+                            BCC2_calc = 0x00;
+                        }
+                        else {
+                            packet[index] = buf[0];
+                            BCC2_calc = buf[0];
+                            index++;
+                            state = READ_INFO;
+                        }
+                    }
+                }
+                else if(buf[0]==FLAG) {
+                    STOP = TRUE;
+                    // BCC2 checks off as true, we received the flag, can send RR message and return
+                }
+                else if(buf[0]==0x00) { // BCC2 may be 0x00, so we must check for that
+                    state = STANDBY;
+                    BCC2_calc = 0x00;
+                }
+                else {
+                    state = READ_INFO;
+                    packet[index] = standby_byte
+                }
+                break;
+            case awaiting_FLAG:
+                if(buf[0]==FLAG) {
+                    SET = TRUE;
+                    state = START;
+                }
+                else {
+                    state = START;
+                }
+                break;
+            case awaiting_FLAG_DISC:
+                if(buf[0]==FLAG) {
+                    DISC = TRUE;
+                    //send its own DISC message
+                    state = START;
+                }
+                else {
+                    state = START;
+                }
+                break;
+            default:
+                break;
+        }
+    }
 
     return 0;
 }
